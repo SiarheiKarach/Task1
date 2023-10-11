ffd46308f04ef774e9e58dbdaf2dd29d13744b8d
SiarheiKarach <147544633+SiarheiKarach@users.noreply.github.com>
2023-10-11 23:50:40 +0200

    Update projectMain.java

--- src/main/java/edu/itacademy/first/main/projectMain.java
+++ src/main/java/edu/itacademy/first/main/projectMain.java
@@ -6,6 +6,29 @@ class ProjectMain {
     public static void main(String[] args) {
         Ball ball = new Ball(2, "steel","red", 5.2);
         ball.setRadius(3);
-        System.out.println(ball);
+
+        Ball woodBall = new Ball(3,"wood","brown",7.5);
+        woodBall.setColor("white");
+
+        Ball plasticBall = new Ball(7,"plastic","white",0.7);
+        plasticBall.setWeight(1.7);
+
+        double totalWeight = Math.round(ball.getWeight()+woodBall.getWeight()+plasticBall.getWeight());
+        System.out.println("Total weight of balls is around "+totalWeight+ "kg.");
+
+        if  (ball.getColor()== "white") {
+            System.out.println("Ball is white");
+        }
+
+        if  (woodBall.getColor()== "white") {
+            System.out.println("Woodball is white");
+        }
+
+        if  (plasticBall.getColor()== "white") {
+            System.out.println("Plasticball is white");
+        }
+
+
     }
+
 }
