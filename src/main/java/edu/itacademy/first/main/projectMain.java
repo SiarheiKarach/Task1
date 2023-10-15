860aa5d9600e846a7099ad52857d4904391d25b3
SiarheiKarach <karach19071992@gmail.com>
2023-10-15 14:38:32 +0200

    Update Ball

--- src/main/java/edu/itacademy/first/main/projectMain.java
+++ src/main/java/edu/itacademy/first/main/projectMain.java
@@ -1,11 +1,50 @@
 package edu.itacademy.first.main;
 
 import edu.itacademy.first.entity.Ball;
+import edu.itacademy.first.service.serviceBall;
 
-class ProjectMain {
+class projectMain {
     public static void main(String[] args) {
-        Ball ball = new Ball(2, "steel","red", 5.2);
+        Ball ball = new Ball(2, "steel", "red", 5.2);
         ball.setRadius(3);
-        System.out.println(ball);
+
+        Ball woodBall = new Ball(3, "wood", "brown", 7.5);
+        woodBall.setColor("white");
+
+        Ball plasticBall = new Ball(7, "plastic", "white", 0.7);
+        plasticBall.setWeight(1.7);
+
+//        double totalWeight = Math.round(ball.getWeight() + woodBall.getWeight() + plasticBall.getWeight());
+//        System.out.println("Total weight of balls is around " + totalWeight + "kg.");
+//
+//        if (ball.getColor() == "white") {
+//            System.out.println("Ball is white");
+//        }
+//
+//        if (woodBall.getColor() == "white") {
+//            System.out.println("Woodball is white");
+//        }
+//
+//        if (plasticBall.getColor() == "white") {
+//            System.out.println("Plasticball is white");
+//        }
+        Ball[] balls = new Ball[3];
+        balls [0] = ball;
+        balls [1] = woodBall;
+        balls [2]= plasticBall;
+//        for (int i = 0; i < balls.length; i++) {
+//        System.out.println(balls[i]);}
+        serviceBall service = new serviceBall();
+        double result = service.GetBallWeight(balls);
+        System.out.println("Total weight of balls is around "+ Math.round(result)+ "kg.");
+
+        int result1 = service.GetBallByColor(balls, "red");
+        System.out.println("Numbers of balls colored in chosen color is "+ result1);
+
+
+
+
+
     }
 }
+
---
+++ src/main/java/edu/itacademy/first/service/serviceBall.java
@@ -0,0 +1,22 @@
+package edu.itacademy.first.service;
+
+import edu.itacademy.first.entity.Ball;
+
+public class serviceBall {
+    public double GetBallWeight(Ball[] balls) {
+        double total = 0;
+        for (int i = 0; i < balls.length ; i++) {
+            total += balls[i].getWeight();
+        }
+        return total;
+    }
+    public int GetBallByColor(Ball[] balls, String countColor) {
+        int total1 = 0;
+        for (int i = 0; i < balls.length ; i++) {
+           if (balls[i].getColor().equals(countColor)){
+            total1++;}
+        }
+        return total1;
+    }
+
+}
